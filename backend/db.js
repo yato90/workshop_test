de72c56c56b415d6d451c1bb910c02e43afff7fa
yato90 <zahaoyam@yandex.ru>
2025-05-15 17:03:29 +0300

    small fix

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,19 @@
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
+// });
+
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+// };
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false,  // Обычно для Render SSL нужен
+  },
 });
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
