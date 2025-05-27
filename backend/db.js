06010729587055b8b05ac16bcce8767eaa87f9f6
yato90 <zahaoyam@yandex.ru>
2025-05-27 19:21:16 +0300

    add new db

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,23 @@
  const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+// const isProduction = process.env.NODE_ENV === "production";
 
 // const pool = new Pool({
-//   connectionString: 'postgresql://test_db_m6ft_user:zxF2IhIvze5T1hKhBfoRXPb4pyL2ZALw@dpg-d0qrhpre5dus739rfgsg-a:5432/test_db_m6ft',
-//   ssl: {
-//     rejectUnauthorized: false, // для Render нужно
-//   },
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
 // });
 
 // module.exports = {
 //   query: (text, params) => pool.query(text, params),
 // };
+
+const pool = new Pool({
+  connectionString: 'postgresql://work_db_u963_user:tIG4Pcz6q4keJEPv9QdoZFLiALzIj9LI@dpg-d0qu69umcj7s73edldn0-a:5432/work_db_u963',
+  ssl: {
+    rejectUnauthorized: false, // для Render нужно
+  },
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
